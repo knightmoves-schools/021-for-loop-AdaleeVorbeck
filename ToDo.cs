5c3e72c3e03b30047b3a93c25d37ed34ad5c34fa
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-18 14:15:19 -0500

    we are expirimenting with ai today :D

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,8 @@
 public class ToDo
 {
     // Add your code here
+    public method MarkAsDone
 }
+
+//should create a method named MarkAsDone that takes an array of todos and returns the modified string array
+//should loop through the todos and prepend the string 'done - ' to each todo

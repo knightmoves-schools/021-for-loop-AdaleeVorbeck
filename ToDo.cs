586e1698357f8b7889abea691b39ed4461d28d58
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-19 14:12:33 -0500

    I WILL SOFTEN EVERY EDGE, HOLD THE WORLD TO IT'S BEST

--- ToDo.cs
+++ ToDo.cs
@@ -1,8 +1,16 @@
-﻿namespace knightmoves;
+namespace knightmoves;
+
 public class ToDo
 {
     // Add your code here
-    public method MarkAsDone
+    public string[] MarkAsDone(string[] todos)
+    {
+        for (int i = 0; i < todos.Length; i++)
+        {
+            todos[i] = "done - " + todos[i];
+        }
+        return todos;
+    }
 }
 
 //should create a method named MarkAsDone that takes an array of todos and returns the modified string array
